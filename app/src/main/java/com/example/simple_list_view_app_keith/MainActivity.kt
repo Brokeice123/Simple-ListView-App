d9c1a483006dfb1244512f188ee7de176700d6ba
Brokeice123 <keithmarkomen@gmail.com>
2023-04-13 12:16:00 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/simple_list_view_app_keith/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.simple_list_view_app_keith
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
